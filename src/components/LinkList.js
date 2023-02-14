ddaa0235db4eef4ea8ee7533f35f7d2a25929bd8
bhar4t <seizedbharat@gmail.com>
2023-02-14 09:11:49 +0530

    subscriptions added

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -27,12 +27,82 @@ export const FEED_QUERY = gql`
 `
 ;
 
-const LinkList = () => {
-  const { data } = useQuery(FEED_QUERY);
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
 
-  React.useEffect(() => {
-    console.log(data)
-  })
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+const LinkList = () => {
+    const {
+        data,
+        loading,
+        error,
+        subscribeToMore
+      } = useQuery(FEED_QUERY);
+    
+      subscribeToMore({
+        document: NEW_LINKS_SUBSCRIPTION,
+        // document: NEW_VOTES_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev;
+          const newLink = subscriptionData.data.newLink;
+          const exists = prev.feed.links.find(
+            ({ id }) => id === newLink.id
+          );
+          if (exists) return prev;
+      
+          return Object.assign({}, prev, {
+            feed: {
+              links: [newLink, ...prev.feed.links],
+              count: prev.feed.links.length + 1,
+              __typename: prev.feed.__typename
+            }
+          });
+        }
+      });;
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ import {
   createHttpLink,
   InMemoryCache
 } from '@apollo/client';
+
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -28,8 +33,30 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
 
